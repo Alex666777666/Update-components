56417a97cdfe8446cd8feed8891f36cfda34cdc9
Alex666777666 <aleksfnaf@gmail.com>
2023-05-03 17:37:22 +0300

    Update-components

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import './index.scss'
+
+ReactDOM.render(<App number={0} />, document.querySelector('#root'))
